23b372a0918ff233d6e8b6d9faac4ba3fdfe1e5f
Reyhaneh Ezatpanah <ezatpanah@gmail.com>
2022-07-25 10:17:39 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ezatpanah/taptargetview_youtube/MainActivity.kt
+++ app/src/main/java/com/ezatpanah/taptargetview_youtube/MainActivity.kt
@@ -13,25 +13,29 @@ import com.getkeepsafe.taptargetview.TapTargetView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    lateinit var prefManager : SharedPreferences
+
+        lateinit var binding: ActivityMainBinding
+    lateinit var sharedPref: SharedPreferences
+    lateinit var prefEditor :  SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        prefManager = PreferenceManager.getDefaultSharedPreferences(this)
+
+        sharedPref = getSharedPreferences("didShowPrompt", MODE_PRIVATE)
+        prefEditor =sharedPref.edit()
 
         showFabPrompt()
 
     }
 
-    private fun showFabPrompt(){
-        if (!prefManager.getBoolean("didShowPrompt", false)){
+    private fun showFabPrompt() {
+        if (!sharedPref.getBoolean("didShowPrompt", false)) {
             TapTargetView.showFor(this,  // `this` is an Activity
                 TapTarget.forView(binding.btnFab, "This is a target", "We have the best targets, believe me") // All options below are optional
                     .tintTarget(false)
-                    .outerCircleColor(R.color.holo_purple),  // Specify the target radius (in dp)
+                    .outerCircleColor(R.color.purple_200),  // Specify the target radius (in dp)
                 object : TapTargetView.Listener() {
                     override fun onTargetClick(view: TapTargetView) {
                         super.onTargetClick(view)
@@ -41,19 +45,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showButtonPrompt(){
+    private fun showButtonPrompt() {
         TapTargetView.showFor(this,
             TapTarget.forView(binding.btnTest, "This is a Test Button", "We have the best targets, believe me") // All options below are optional
                 .tintTarget(false)
-                .outerCircleColor(R.color.holo_purple),
+                .outerCircleColor(R.color.purple_200),
             object : TapTargetView.Listener() {
                 override fun onTargetClick(view: TapTargetView) {
                     super.onTargetClick(view)
-                    val prefEditor = prefManager.edit()
+
+                    val prefEditor = sharedPref.edit()
                     prefEditor.putBoolean("didShowPrompt", true)
                     prefEditor.apply()
                 }
             })
     }
-
 }
